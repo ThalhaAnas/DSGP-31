dbff6292f465a06dbc2a14fc5c96d6fc648d3f66
ThalhaAnas <thalhaanastwo@gmail.com>
2026-01-30 11:11:32 +0530

    Random forest classification updated

--- classification_random_forest.py
+++ classification_random_forest.py
@@ -4,9 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 
-df = pd.read_csv("component1_enriched_fixed.csv")
+df = pd.read_csv("component1_merged.csv")
 
-features = ["vehicle_count", "avg_waiting_time", "avg_time_loss"]
+df["system_encoded"] = df["system_type"].map({
+    "fixed": 0,
+    "adaptive": 1
+})
+
+features = ["vehicle_count", "avg_waiting_time", "avg_time_loss", "system_encoded"]
 
 X = df[features]
 y = df["is_high_traffic"]
@@ -16,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 #Train model
 
-rf = RandomForestClassifier(n_estimators=200, max_depth=10, min_samples_split=10,
+rf = RandomForestClassifier(n_estimators=300, max_depth=12, min_samples_split=10,
                             class_weight="balanced", random_state=42, n_jobs=-1)
 
 rf.fit(X_train, y_train)
