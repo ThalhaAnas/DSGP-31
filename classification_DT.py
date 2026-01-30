9aea5b418d11b25ed38a81424b5a2c64f35d632e
ThalhaAnas <thalhaanastwo@gmail.com>
2026-01-30 11:11:11 +0530

    Decision tree classification updated

--- classification_DT.py
+++ classification_DT.py
@@ -4,11 +4,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
-df = pd.read_csv("component1_enriched_fixed.csv")
+df = pd.read_csv("component1_merged.csv")
 
 #select features
 
-features = ["vehicle_count", "avg_waiting_time", "avg_time_loss"]
+df["system_encoded"] = df["system_type"].map({
+    "fixed": 0,
+    "adaptive": 1
+})
+
+features = ["vehicle_count", "avg_waiting_time", "avg_time_loss", "system_encoded"]
 
 X = df[features]
 y = df["is_high_traffic"]
@@ -19,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 #Train DT model
 
 dt = DecisionTreeClassifier(
-    max_depth=5,
+    max_depth=6,
     min_samples_leaf=20,
     class_weight="balanced",
     random_state=42,
@@ -30,16 +35,16 @@ dt.fit(X_train, y_train)
 #Evaluation
 y_pred = dt.predict(X_test)
 
-print("Confusion Matrix")
+print("Confusion Matrix (Decision Tree):")
 print(confusion_matrix(y_test, y_pred))
 
-print("\nClassification Report")
+print("\nClassification Report (Decision Tree):")
 print(classification_report(y_test, y_pred))
 
 #feature importance
 
 importance_df = pd.DataFrame({
-    "feature": features,
+    "Feature": features,
     "Importance": dt.feature_importances_
 }).sort_values(by="Importance", ascending=False)
 
