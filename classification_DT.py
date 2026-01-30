f6d4b44aa5c89da646fdc9efa5ff34f536f30b2b
ThalhaAnas <thalhaanastwo@gmail.com>
2026-01-30 09:35:32 +0530

    Decision tree model evaluation

--- classification_DT.py
+++ classification_DT.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
 
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
@@ -12,10 +11,10 @@ df = pd.read_csv("component1_enriched_fixed.csv")
 features = ["vehicle_count", "avg_waiting_time", "avg_time_loss"]
 
 X = df[features]
-y = df["is_high traffic"]
+y = df["is_high_traffic"]
 
 #train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
 #Train DT model
 
@@ -25,3 +24,24 @@ dt = DecisionTreeClassifier(
     class_weight="balanced",
     random_state=42,
 )
+
+dt.fit(X_train, y_train)
+
+#Evaluation
+y_pred = dt.predict(X_test)
+
+print("Confusion Matrix")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nClassification Report")
+print(classification_report(y_test, y_pred))
+
+#feature importance
+
+importance_df = pd.DataFrame({
+    "feature": features,
+    "Importance": dt.feature_importances_
+}).sort_values(by="Importance", ascending=False)
+
+print("\nFeature Importance:")
+print(importance_df)
