98c5b55454ed6ed89e860591c6dc63f3ddf573c1
ThalhaAnas <thalhaanastwo@gmail.com>
2026-02-01 23:07:47 +0530

    Training linear regression model

--- regression_linear.py
+++ regression_linear.py
@@ -16,3 +16,26 @@ df["system_encoded"] = df["system_type"].map({
     "fixed": 0,
     "adaptive": 1
 })
+
+#features and target
+
+features = [ "vehicle_count", "avg_time_loss", "system_encoded"]
+
+X = df[features]
+y = df["avg_waiting_time"]
+
+#Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+#scale features
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#Train linear regression model
+
+lr = LinearRegression()
+lr.fit(X_train_scaled, y_train)
