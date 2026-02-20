c1203a58afca346cfaea0cbc09ee8ea9a9af966e
Swetha-Dewduni <swethadewduni7@gmail.com>
2026-02-20 19:13:39 +0530

    define Configuration Parameters

--- traffic_simulation/adaptive_equilibrium.py
+++ traffic_simulation/adaptive_equilibrium.py
@@ -1,2 +1,19 @@
 import traci
 import pandas as pd
+
+"""
+ADAPTIVE EQUILIBRIUM-BASED CONTROLLER
+
+- Measures congestion at all junctions
+- Computes network average pressure
+- Adjusts green times to balance load
+"""
+
+SUMO_BINARY = "sumo-gui"
+SUMO_CONFIG = "simulation_map.sumocfg"
+
+SIM_END = 3600
+LOG_INTERVAL = 30
+MAX_ADJUST = 0.05
+MIN_GREEN = 5
+
