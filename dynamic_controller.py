bae55159b747b5aae29904a1deb5745c6bea4ff2
ThalhaAnas <thalhaanastwo@gmail.com>
2026-02-21 13:53:55 +0530

    method to calculate halted vehicles queue per lane group

--- dynamic_controller.py
+++ dynamic_controller.py
@@ -9,3 +9,6 @@ MIN_GREEN = 10
 MAIN_CLEAR_THRESHOLD = 5
 SIDE_QUEUE_THRESHOLD = 8
 SIDE_RELIEF_TIME = 25
+
+def get_lane_queue(lanes):
+    return sum(traci.lane.getLastStepHaltingNumber(l) for l in lanes)
