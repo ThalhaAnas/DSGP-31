f2fbf04463eac52020639db5783779a9b1098661
ThalhaAnas <thalhaanastwo@gmail.com>
2026-03-07 15:05:10 +0530

    split data and train model

--- rf_classifier.py
+++ rf_classifier.py
@@ -16,3 +16,24 @@ df = df.drop(columns=["system_type"], errors="ignore")
 # Define Features & Target
 X = df.drop("is_congested", axis=1)
 y = df["is_congested"]
+
+# Train/Test Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.3,
+    random_state=42,
+    stratify=y
+)
+
+
+# Train Random Forest
+rf = RandomForestClassifier(
+    n_estimators=300,
+    max_depth=10,
+    min_samples_split=10,
+    class_weight="balanced",
+    random_state=42,
+    n_jobs=-1
+)
+
+rf.fit(X_train, y_train)
